bff3b95995de65a3f6e7e9b44b70e2c94e790bc9
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2021-05-28 23:25:29 -0400

    frontend

---
+++ static/client.js
@@ -0,0 +1,50 @@
+const Icon = {
+	// num[0] is meaningless, zero-index is just used as padding so that num[1] is icon "1"
+	num: Array.from(Array(9).keys()).map(String),
+	flag: "flag",
+};
+
+class Square {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+		this.elt = document.createElement("span");
+		this.elt.classList.add("square");
+		this.elt.classList.add("hidden-square");
+	}
+
+	// `setIcon(Icon.flag)` (displays a flag)
+	// `setIcon(Icon.num[1])` (displays a "1")
+	setIcon(iconName) {
+		this.elt.style.backgroundImage = `url("/${iconName}.svg")`;
+	}
+}
+
+class MineField {
+	constructor(boardElement, width, height) {
+		boardElement.innerHTML = "";
+		this.boardElement = boardElement;
+		this.squares = [];
+
+		for (let y = 0; y < height; y++) {
+			let row = [];
+			for (let x = 0; x < width; x++) {
+				let square = new Square(x, y);
+				row.push(square);
+				this.boardElement.appendChild(square.elt);
+			}
+			this.boardElement.appendChild(document.createElement("br"));
+			this.squares.push(row);
+		}
+	}
+
+	getSquare(x, y) {
+		return this.squares[y][x];
+	}
+}
+
+function main() {
+	let boardElt = document.querySelector("#board");
+	let field = new MineField(boardElt, 30, 16);
+}
+addEventListener("load", main);
